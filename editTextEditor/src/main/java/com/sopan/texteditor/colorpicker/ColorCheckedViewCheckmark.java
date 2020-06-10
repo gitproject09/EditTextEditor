d670446cb4787c195898f0315dfc935e2cb000a1
Sopan_Home <spn.ahmed92@gmail.com>
2020-06-10 17:44:20 +0600

    Add color picker and changes directory name

---
+++ editTextEditor/src/main/java/com/sopan/texteditor/colorpicker/ColorCheckedViewCheckmark.java
@@ -0,0 +1,32 @@
+package com.sopan.texteditor.colorpicker;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+
+import com.sopan.texteditor.R;
+
+/**
+ * Created by Sopan on 2018/3/19.
+ */
+
+public class ColorCheckedViewCheckmark extends AppCompatImageView {
+
+    private Context mContext;
+    private int mSize;
+
+    public ColorCheckedViewCheckmark(Context context, int size) {
+        super(context);
+        this.mContext = context;
+        this.mSize = size;
+        initView();
+    }
+
+    private void initView() {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(mSize, mSize);
+        layoutParams.gravity = Gravity.CENTER;
+        this.setLayoutParams(layoutParams);
+        this.setImageResource(R.drawable.check_mark);
+    }
+}
---
+++ editTextEditor/src/main/java/com/sopan/texteditor/colorpicker/ColorPickerListener.java
@@ -0,0 +1,9 @@
+package com.sopan.texteditor.colorpicker;
+
+/**
+ * Created by Sopan on 2018/3/6.
+ */
+
+public interface ColorPickerListener {
+    public void onPickColor(int color);
+}
